1194809a79f256ae5f0415c1ec46cd14b4ba7389
Karim Guettech <karimguettech@gmail.com>
2023-02-25 13:16:24 +0100

    added persist

--- src/index.js
+++ src/index.js
@@ -3,16 +3,16 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import {store,persistor} from "./redux/store";
+import { store, persistor } from "./redux/store";
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/es/integration/react";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <Provider store={store}>
     <PersistGate persistor={persistor}>
-    <App /> </PersistGate>
+      <App />
+    </PersistGate>
   </Provider>
 );
-
 
 reportWebVitals();
